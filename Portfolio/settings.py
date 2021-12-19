37fbf7112053f03bd20d4b8fbd58f1913b53acdb
jlcaw <jlcawarner2011@yahoo.com>
2021-12-19 11:35:33 -0700

    removed include module

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import include
 import django_heroku
 import dj_database_url
 from decouple import config
